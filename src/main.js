d5c0e9995d2da1dc05b2fd48c1b1dd4c5e066503
Vladislav Flicker <flickervladislav@gmail.com>
2021-05-03 19:17:55 +0300

    Render sort on the main page

--- src/main.js
+++ src/main.js
@@ -1,16 +1,21 @@
 import { creatTripInfoTemplate } from './view/trip-info';
 import { createMenuTemplate } from './view/menu';
 import { createFiltersTemplate } from './view/filters';
+import { createSortTemplate } from './view/sort';
 
 const render = (container, block, position) => {
   container.insertAdjacentHTML(position, block);
 };
 
+// Header
 const containerTripMain = document.querySelector('.trip-main');
+const containerMenu = containerTripMain.querySelector('.trip-controls__navigation');
+const containerFilter = containerTripMain.querySelector('.trip-controls__filters');
+
 render(containerTripMain, creatTripInfoTemplate(), 'afterbegin');
-
-const containerMenu = document.querySelector('.trip-controls__navigation');
 render(containerMenu, createMenuTemplate(), 'beforeend');
-
-const containerFilter = document.querySelector('.trip-controls__filters');
 render(containerFilter, createFiltersTemplate(), 'beforeend');
+
+// Main
+const containerTripEvents = document.querySelector('.trip-events');
+render(containerTripEvents, createSortTemplate(), 'beforeend');
