7fdf42d2c61940a8205071c654470e55e40700fd
Vladislav Flicker <flickervladislav@gmail.com>
2021-05-05 21:23:25 +0300

    Refactor

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import EventListView from './view/event-list';
 import EventItemView from './view/event-item';
 import EventItemEditView from './view/event-item-edit';
 import { getEvents } from './mock/event';
-import { render } from './utils/render';
+import { render, RenderPosition } from './utils/render';
 
 const EVENT_COUNT = 5;
 const events = getEvents(EVENT_COUNT);
@@ -16,18 +16,18 @@ const containerTripMain = document.querySelector('.trip-main');
 const containerMenu = containerTripMain.querySelector('.trip-controls__navigation');
 const containerFilter = containerTripMain.querySelector('.trip-controls__filters');
 
-render(containerTripMain, new TripInfoView(events).getElement(), 'afterbegin');
-render(containerMenu, new MenuView().getElement(), 'beforeend');
-render(containerFilter, new FilterView().getElement(), 'beforeend');
+render(containerTripMain, new TripInfoView(events).getElement(), RenderPosition.AFTERBEGIN);
+render(containerMenu, new MenuView().getElement(), RenderPosition.BEFOREEND);
+render(containerFilter, new FilterView().getElement(), RenderPosition.BEFOREEND);
 
 // Main
 const containerTripEvents = document.querySelector('.trip-events');
-render(containerTripEvents, new SortView().getElement(), 'beforeend');
-render(containerTripEvents, new EventListView().getElement(), 'beforeend');
+render(containerTripEvents, new SortView().getElement(), RenderPosition.BEFOREEND);
+render(containerTripEvents, new EventListView().getElement(), RenderPosition.BEFOREEND);
 
 const containerEventList = document.querySelector('.trip-events__list');
-render(containerEventList, new EventItemEditView(events[0]).getElement(), 'beforeend');
+render(containerEventList, new EventItemEditView(events[0]).getElement(), RenderPosition.BEFOREEND);
 
 events.forEach((event) => {
-  render(containerEventList, new EventItemView(event).getElement(), 'beforeend');
+  render(containerEventList, new EventItemView(event).getElement(), RenderPosition.BEFOREEND);
 });
