dfc92ca1a6be1414200155828ab8b4d86231a37e
Vladislav Flicker <flickervladislav@gmail.com>
2021-05-05 17:14:24 +0300

    Add render funcrions

--- src/utils/render.js
+++ src/utils/render.js
@@ -1,3 +1,42 @@
-export const renderTemplate = (container, block, position) => {
+const RenderPosition = {
+  BEFOREBEGIN: 'beforebegin',
+  AFTERBEGIN: 'afterbegin',
+  BEFOREEND: 'beforeend',
+  AFTEREND: 'afterend',
+};
+
+const renderElement = (container, element, place) => {
+  switch (place) {
+    case RenderPosition.BEFOREBEGIN:
+      container.before(element);
+      break;
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+    case RenderPosition.AFTEREND:
+      container.after(element);
+      break;
+  }
+};
+
+const createElement = (template) => {
+  const newElement = document.createElement('div');
+  newElement.innerHTML = template;
+
+  return newElement.firstChild;
+};
+
+const renderTemplate = (container, block, position) => {
   container.insertAdjacentHTML(position, block);
+};
+
+
+export {
+  RenderPosition,
+  renderElement,
+  createElement,
+  renderTemplate
 };
