11ba9945ff81c1acda8b26516bae87b0d9a87f86
Vladislav Flicker <flickervladislav@gmail.com>
2021-07-02 19:24:30 +0300

    Refactor model

--- src/model/events.js
+++ src/model/events.js
@@ -17,22 +17,6 @@ export default class Events extends Observer {
     return this._events;
   }
 
-  updateEvent(updateType, update) {
-    const index = this._events.findIndex((event) => event.id === update.id);
-
-    if (index === -1) {
-      throw new Error('Can\'t update unexisting event');
-    }
-
-    this._events = [
-      ...this._events.slice(0, index),
-      update,
-      ...this._events.slice(index + 1),
-    ];
-
-    this._notify(updateType, update);
-  }
-
   addEvent(updateType, update) {
     this._events = [
       update,
@@ -55,6 +39,22 @@ export default class Events extends Observer {
     ];
 
     this._notify(updateType);
+  }
+
+  updateEvent(updateType, update) {
+    const index = this._events.findIndex((event) => event.id === update.id);
+
+    if (index === -1) {
+      throw new Error('Can\'t update unexisting event');
+    }
+
+    this._events = [
+      ...this._events.slice(0, index),
+      update,
+      ...this._events.slice(index + 1),
+    ];
+
+    this._notify(updateType, update);
   }
 
   static adaptToClient(event) {
