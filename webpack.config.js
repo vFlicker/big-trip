7c2fcaf9ad25b351d2454a580a3e34515f30ee7f
Vladislav <flickervladislav@gmail.com>
2022-08-24 17:18:35 +0300

    chore: update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,30 @@
 const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
   },
   devtool: 'source-map',
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: 'public' }],
+    }),
+  ],
   devServer: {
-    contentBase: path.resolve(__dirname, 'public'),
-    watchContentBase: true
+    static: path.resolve(__dirname, 'public'),
+    open: true,
   },
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: ['babel-loader']
+      },
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader']
